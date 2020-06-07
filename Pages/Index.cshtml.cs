cf455ff964dbddb9fdca1b04e343ba3e55359708
SwelloLee <48401741+SwelloLee@users.noreply.github.com>
2020-06-07 22:55:46 +0800

    Initial Commit

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+
+namespace BugTracker.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(SignInManager<IdentityUser> signInManager, ILogger<IndexModel> logger)
+        {
+            _signInManager = signInManager;
+            _logger = logger;
+        }
+
+        public IActionResult OnGet()
+        {
+            if(_signInManager.IsSignedIn(User)){
+                return RedirectToPage("/Index", new { area = "Dashboard"});
+            }
+
+            return null;
+        }
+
+        public async Task<IActionResult> OnPost(string returnUrl = null)
+        {
+            await _signInManager.SignOutAsync();
+            _logger.LogInformation("User logged out.");
+            if (returnUrl != null)
+            {
+                return LocalRedirect(returnUrl);
+            }
+            else
+            {
+                return RedirectToPage();
+            }
+        }
+    }
+}
